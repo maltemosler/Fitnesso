a369eb124fed511c385bbb7bee20a1244cfa6384
malte <deemonrider@gmx.de>
2020-12-07 16:33:05 +0100

    added db

--- API/models.py
+++ API/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Trainer(models.Model):
+    vorname = models.CharField(max_length=32, null=True)
+    nachname = models.CharField(max_length=32, null=True)
+
+
+class FitnessoUser(models.Model):
+    trainer = models.ForeignKey(Trainer, models.CASCADE, null=True)
+    vorname = models.CharField(max_length=32, null=True)
+    nachname = models.CharField(max_length=32, null=True)
+
+
+class HauptZiel(models.Model):
+    user = models.ForeignKey(FitnessoUser, models.CASCADE, null=True)
+    ziel = models.CharField(max_length=256, null=True)
+    status = models.BooleanField(default=True, null=False)
+
+
+class Unterziel(models.Model):
+    hauptziel = models.ForeignKey(HauptZiel, models.CASCADE, null=True)
+    ziel = models.CharField(max_length=128, null=True)
+    status = models.BooleanField(default=True, null=False)
