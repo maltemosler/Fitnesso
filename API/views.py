7fc4743d3970dc2a807487605710072160b0f459
malte <deemonrider@gmx.de>
2020-12-07 19:01:22 +0100

    ziele can now show the goals userlist now also shows users

--- API/views.py
+++ API/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from API.models import FitnessoUser, HauptZiel, Unterziel
+
 
 def global_context(request, context):
     return context
@@ -7,6 +9,19 @@ def global_context(request, context):
 
 def home_view(request):
     context = {'title': "Fitnesso | Home"}
+    if request.user.is_authenticated:
+        if request.user.fitnessouser.is_trainer:
+            context["users"] = FitnessoUser.objects.filter()
+        else:
+            ziele = []
+            for hauptziel in HauptZiel.objects.filter():
+                unterziele = []
+                for unterziel in Unterziel.objects.filter(hauptziel=hauptziel):
+                    unterziele.append({"id": unterziel.id, "ziel": unterziel.ziel})
+                if not hauptziel.id in ziele:
+                    ziele.append({"id": hauptziel.id, "ziel": hauptziel.ziel, "unterziele": unterziele})
+
+            context["ziele"] = ziele
     return render(request, "home.html", context=global_context(request, context))
 
 
